e23fd31b1c944611f93123d12211c2db36fe282f
Lucie <lucie.menard@partnre.com>
2024-04-22 09:24:31 +0200

    feat: implementation in a single file components

---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,38 @@
+<script setup>
+// EventCard
+
+import { ref } from "vue";
+
+defineProps({
+  restaurant: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="restaurant-card">
+    <h2>{{ restaurant.title }}</h2>
+    <p>{{ restaurant.description }}</p>
+    <p>{{ restaurant.location }}</p>
+    <span>
+      Recommended : {{ restaurant.recommended }} | ever tried :
+      {{ restaurant.tried }}</span
+    >
+  </div>
+</template>
+
+<style scoped>
+.restaurant-card {
+  padding: 20px;
+  width: 350px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.restaurant-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/views/RestaurantListView.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from "vue";
+import RestaurantCard from "@/components/RestaurantCard.vue";
+
+const restaurant = ref([
+  {
+    id: 123,
+    category: "mexican",
+    title: "Mexico",
+    description: "A Mexican restaurant.",
+    tag: [],
+    location: "Baulieu",
+    date: "January 28, 2022",
+    time: "12:00",
+    tried: false,
+    recommended: "Flo",
+  },
+  {
+    id: 456,
+    category: "europeen",
+    title: "Satio",
+    description: "Triple S.",
+    tag: [],
+    location: "Vasselot",
+    date: "January 91, 2022",
+    time: "12:00",
+    tried: true,
+    recommended: "Instagram",
+  },
+  {
+    id: 789,
+    category: "asian",
+    title: "Table Rouge",
+    description: "To have yakitori at home.",
+    tag: [],
+    location: "Online & ?",
+    date: "February 28, 2022",
+    time: "12:00",
+    tried: true,
+    recommended: "Audrey",
+  },
+]);
+</script>
+
+<template>
+  <h1>Le temple du palais</h1>
+
+  <div class="restaurants">
+    <RestaurantCard
+      v-for="resto in restaurant"
+      :key="restaurant.id"
+      :restaurant="resto" />
+  </div>
+</template>
+
+<style scoped>
+.restaurants {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
